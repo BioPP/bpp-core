bf4844a012c853257bae8db25a9e7a965ee786ac
Julien Dutheil <jy.dutheil@gmail.com>
2010-10-30 16:47:12 +0200

    Fixed starting time.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -5,7 +5,7 @@
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
 set(CTEST_PROJECT_NAME "bpp-core")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "biopp.univ-montp2.fr")
