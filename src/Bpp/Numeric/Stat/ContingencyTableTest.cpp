4b6796693a4138dcbb08219d9a171a07152c536d
Julien Y. Dutheil <jy.dutheil@gmail.com>
2010-12-10 15:38:05 +0100

    Added chisq test on contingency table.

---
+++ src/Bpp/Numeric/Stat/ContingencyTableTest.cpp
@@ -0,0 +1,95 @@
+//
+// File: ContingencyTableTest.cpp
+// Created by: Julien Dutheil
+// Created on: Thu Dec 09 14:20 2010
+//
+
+/*
+Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
+
+This software is a computer program whose purpose is to provide classes
+for numerical calculus.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#include "ContingencyTableTest.h"
+
+#include "../../App/ApplicationTools.h"
+#include "../VectorTools.h"
+#include "../RandomTools.h"
+
+#include <iostream>
+
+using namespace bpp;
+using namespace std;
+
+ContingencyTableTest::ContingencyTableTest(const std::vector< std::vector<unsigned int> >& table):
+  statistic_(0),
+  pvalue_(0),
+  df_(0),
+  margin1_(table.size()),
+  margin2_(0)
+{
+  //Compute marginals:
+  unsigned int n = table.size();
+  if (n < 2)
+    throw Exception("ContingencyTableTest. Table size should be at least 2x2!");
+  unsigned int m = table[0].size();
+  if (m < 2)
+    throw Exception("ContingencyTableTest. Table size should be at least 2x2!");
+  margin2_.resize(m);
+  for (unsigned int j = 0; j < m; ++j)
+    margin2_[j] = 0.;
+  bool test = false;
+  for (size_t i = 0; i < n; ++i) {
+    if (table[i].size() != m)
+      throw Exception("ContingencyTableTest. Input array has non-homogeneous dimensions!");
+    for (size_t j = 0; j < m; ++j) {
+      double c = table[i][j];
+      if (c <= 5) test = true;
+      margin1_[i] += c;
+      margin2_[j] += c;
+    }
+  }
+  if (test)
+    ApplicationTools::displayWarning("Unsufficient observations, p-value might be incorrect.");
+
+  double tot = VectorTools::sum(margin1_);
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j < m; ++j) {
+      double c = table[i][j];
+      double e = margin1_[i] * margin2_[j] / static_cast<double>(tot);
+      statistic_ += std::pow(c - e, 2.) / e;
+    }
+  }
+
+  df_ = static_cast<double>((m - 1) * (n - 1));
+  //Compute p-value:
+  pvalue_ = 1. - RandomTools::pChisq(statistic_, df_);
+}
+
---
+++ src/Bpp/Numeric/Stat/ContingencyTableTest.h
@@ -0,0 +1,89 @@
+//
+// File: ContingencyTableTest.h
+// Created by: Julien Dutheil
+// Created on: Thu Dec 09 14:20 2010
+//
+
+/*
+Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
+
+This software is a computer program whose purpose is to provide classes
+for numerical calculus.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#ifndef _CONTINGENCYTABLETEST_H_
+#define _CONTINGENCYTABLETEST_H_
+
+#include "StatTest.h"
+
+//From the STL:
+#include <vector>
+
+namespace bpp
+{
+
+/**
+ * @brief Implements tests on contingency tables.
+ *
+ * Performs a chi square test on contingency tables.
+ */
+class ContingencyTableTest:
+  public virtual StatTest
+{
+  private:
+    double statistic_;
+    double pvalue_;
+    double df_;
+    std::vector<double> margin1_;
+    std::vector<double> margin2_;
+
+  public:
+    ContingencyTableTest(const std::vector< std::vector<unsigned int> >& table);
+    virtual ~ContingencyTableTest() {}
+
+#ifndef NO_VIRTUAL_COV
+    ContingencyTableTest*
+#else
+    Clonable*
+#endif
+    clone() const { return new ContingencyTableTest(*this); }
+
+  public:
+    std::string getName() const { return "Test on contingency table."; }
+    double getStatistic() const { return statistic_; }
+    double getPValue() const { return pvalue_; }
+    double getDegreesOfFreedom() const { return df_; }
+
+};
+
+} //end of namespace bpp.
+
+#endif //_CONTINGENCYTABLETEST_H_
+
+
