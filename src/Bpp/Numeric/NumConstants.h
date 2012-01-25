793f98553a73dff3afc958a05e36b11649a24ae8
Laurent Gueguen <Laurent.Gueguen@univ-lyon1.fr>
2012-01-25 15:24:24 +0100

    new NumConstants

--- src/Bpp/Numeric/NumConstants.h
+++ src/Bpp/Numeric/NumConstants.h
@@ -5,36 +5,36 @@
 //
 
 /*
-Copyright or © or Copr. CNRS, (November 17, 2004)
+  Copyright or © or Copr. CNRS, (November 17, 2004)
 
-This software is a computer program whose purpose is to provide classes
-for numerical calculus. This file is part of the Bio++ project.
+  This software is a computer program whose purpose is to provide classes
+  for numerical calculus. This file is part of the Bio++ project.
 
-This software is governed by the CeCILL  license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
-modify and/ or redistribute the software under the terms of the CeCILL
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+  This software is governed by the CeCILL  license under French law and
+  abiding by the rules of distribution of free software.  You can  use, 
+  modify and/ or redistribute the software under the terms of the CeCILL
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info". 
 
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited
-liability. 
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability. 
 
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or 
+  data to be ensured and,  more generally, to use and operate it in the 
+  same conditions as regards security. 
 
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL license and that you accept its terms.
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL license and that you accept its terms.
 */
 
 #ifndef _NUMCONSTANTS_H_
@@ -42,11 +42,11 @@ knowledge of the CeCILL license and that you accept its terms.
 
 namespace bpp {
 
-/**
- * @brief this static class contains several useful constant values.
- */
-class NumConstants
-{
+  /**
+   * @brief this static class contains several useful constant values.
+   */
+  class NumConstants
+  {
   public:
     /**
      * @name Golden ratio.
@@ -60,13 +60,22 @@ class NumConstants
     static const double GOLDEN_RATIO_C;
     /** @} */
 
+    static const double MEGA;
+    static const double KILO;
+    static const double DECI;
+    static const double CENTI;
+    static const double MILLI;
+    static const double MICRO;
+    static const double NANO;
+    static const double PICO;
+    
     static const double SMALL;
     static const double TINY;
     static const double VERY_TINY;
     static const double VERY_BIG;
 
     static const double PI;
-};
+  };
 
 }//end of namespace bpp.
 
