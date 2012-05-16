e812332a8820641313d7a08f24338b518449efa2
Julien Y. Dutheil <jy.dutheil@gmail.com>
2012-05-16 15:55:19 +0200

    Modified copyright.

--- src/Bpp/Numeric/Prob/DiscreteDistribution.h
+++ src/Bpp/Numeric/Prob/DiscreteDistribution.h
@@ -5,7 +5,7 @@
 //
 
 /*
-  Copyright or © or Copr. CNRS, (November 19, 2004)
+  Copyright or © or Copr. Bio++ Development Team, (November 19, 2004)
 
   This software is a computer program whose purpose is to provide classes
   for numerical calculus.
