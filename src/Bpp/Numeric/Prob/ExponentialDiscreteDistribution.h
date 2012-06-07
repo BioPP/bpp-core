288d831d28dbb2f4e98126dea8ce197da9082a91
Laurent Gueguen <Laurent.Gueguen@univ-lyon1.fr>
2012-06-07 16:50:09 +0200

    correction of TruncExponentialDD

--- src/Bpp/Numeric/Prob/ExponentialDiscreteDistribution.h
+++ src/Bpp/Numeric/Prob/ExponentialDiscreteDistribution.h
@@ -55,8 +55,6 @@ class ExponentialDiscreteDistribution :
   public AbstractDiscreteDistribution
 {
 protected:
-  IntervalConstraint* lambdaConstraint_;
-
   double lambda_;
 
 public:
@@ -70,12 +68,11 @@ public:
    *
    */
 
-  ExponentialDiscreteDistribution(unsigned int n, double lambda = 1., std::string prefix = "Exponential.");
+  ExponentialDiscreteDistribution(unsigned int n, double lambda = 1.);
 
   ExponentialDiscreteDistribution(const ExponentialDiscreteDistribution& dist) :
     AbstractParameterAliasable(dist),
     AbstractDiscreteDistribution(dist),
-    lambdaConstraint_(dynamic_cast<IntervalConstraint*>(dist.lambdaConstraint_->clone())),
     lambda_(dist.lambda_)
   {}
 
@@ -83,12 +80,11 @@ public:
   {
     AbstractParameterAliasable::operator=(dist);    
     AbstractDiscreteDistribution::operator=(dist);
-    lambdaConstraint_ = dynamic_cast<IntervalConstraint*>(dist.lambdaConstraint_->clone());
     lambda_ = dist.lambda_;
     return *this;
   }
 
-  virtual ~ExponentialDiscreteDistribution();
+  ~ExponentialDiscreteDistribution(){};
 
   ExponentialDiscreteDistribution* clone() const { return new ExponentialDiscreteDistribution(*this); }
 
--- src/Bpp/Numeric/Prob/TruncatedExponentialDiscreteDistribution.h
+++ src/Bpp/Numeric/Prob/TruncatedExponentialDiscreteDistribution.h
@@ -47,18 +47,30 @@
 namespace bpp
 {
 /**
- * @brief Discretized Truncated (on the right) Exponential distribution.
+ * @brief Discretized Truncated (on the right) Exponential
+ * distribution, where the probabilities are given the exponential,
+ * conditioned by the upper limit.
  *
  * This distribution has two parameters: the traditional exponential law parameter,
  * and the abscissa of the truncation. The distribution will be truncated on the right
  * of this point.
  */
+  
 class TruncatedExponentialDiscreteDistribution :
-  public ExponentialDiscreteDistribution
+  public AbstractDiscreteDistribution
 {
 protected:
+  double lambda_;
+
   double tp_;
 
+  /*
+   * Probability of the condition given x < tp_.
+   *
+   */
+   
+  double cond_;
+  
 public:
   /**
    * @brief Build a new truncated exponential discrete distribution.
@@ -74,23 +86,62 @@ public:
 
   TruncatedExponentialDiscreteDistribution(const TruncatedExponentialDiscreteDistribution& dist) :
     AbstractParameterAliasable(dist),
-    ExponentialDiscreteDistribution(dist),
-    tp_(dist.tp_) {}
+    AbstractDiscreteDistribution(dist),
+    lambda_(dist.lambda_),
+    tp_(dist.tp_),
+    cond_(dist.cond_)
+  {}
 
   TruncatedExponentialDiscreteDistribution& operator=(const TruncatedExponentialDiscreteDistribution& dist)
   {
     AbstractParameterAliasable::operator=(dist);
-    ExponentialDiscreteDistribution::operator=(dist);
+    AbstractDiscreteDistribution::operator=(dist);
+    lambda_= dist.lambda_;
     tp_ = dist.tp_;
+    cond_ = dist.cond_;
     return *this;
   }
 
-  virtual ~TruncatedExponentialDiscreteDistribution();
+  ~TruncatedExponentialDiscreteDistribution(){};
 
   TruncatedExponentialDiscreteDistribution* clone() const { return new TruncatedExponentialDiscreteDistribution(*this); }
 
 public:
   void fireParameterChanged(const ParameterList& parameters);
+
+  double randC() const throw (Exception)
+  {
+    double x = RandomTools::randExponential(1. / getParameterValue("lambda"));
+    while (!intMinMax_.isCorrect(x))
+      x = RandomTools::randExponential(1. / getParameterValue("lambda"));
+
+    return x;
+  }
+
+  double pProb(double x) const
+  {
+    if (x>=tp_)
+      return 1.;
+    else
+      return (1. - exp(-lambda_ * x))/cond_;
+  }
+
+  double qProb(double x) const
+  {
+    if (x==1)
+      return tp_;
+    else
+      return -log(1. - cond_*x) / lambda_;
+  }
+
+  double Expectation(double a) const
+  {
+    if (a<tp_)
+      return (1. / lambda_ - exp(-a * lambda_) * (a + 1. / lambda_))/cond_;
+    else
+      return (1. / lambda_ - exp(-tp_ * lambda_) * (tp_ + 1. / lambda_))/cond_;
+      
+  }
 
   void restrictToConstraint(const Constraint& c);
 };
