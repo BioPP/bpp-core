54f91284f63d4707b79fbaed8c8eff683e510afa
Julien Y. Dutheil <jy.dutheil@gmail.com>
2013-01-25 09:32:08 +0100

    Fixed includes.

--- test/test_text_tools.cpp
+++ test/test_text_tools.cpp
@@ -37,7 +37,8 @@ The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
 */
 
-#include <Bpp/Text.all>
+#include <Bpp/Text/TextTools.h>
+#include <Bpp/Text/StringTokenizer.h>
 #include <iostream>
 
 using namespace bpp;
