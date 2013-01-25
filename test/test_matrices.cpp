234956d2d5bcc040bef594b81efc2582299cf341
Julien Y. Dutheil <jy.dutheil@gmail.com>
2013-01-25 09:42:25 +0100

    Another include fixed.

--- test/test_matrices.cpp
+++ test/test_matrices.cpp
@@ -37,7 +37,8 @@ The fact that you are presently reading this means that you have had
 knowledge of the CeCILL license and that you accept its terms.
 */
 
-#include <Bpp/Numeric/Matrix.all>
+#include <Bpp/Numeric/Matrix/Matrix.h>
+#include <Bpp/Numeric/Matrix/MatrixTools.h>
 #include <vector>
 #include <iostream>
 
