fa14b87a959dcb30f54e24d889d53d33c547ea84
Julien Dutheil <jy.dutheil@gmail.com>
2014-09-16 21:38:40 +0200

    Ran uncrustify.

--- src/Bpp/Utils/AttributesTools.cpp
+++ src/Bpp/Utils/AttributesTools.cpp
@@ -5,37 +5,37 @@
 //
 
 /*
-Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
+   Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
 
-This software is a computer program whose purpose is to provide basal and 
-utilitary classes. This file belongs to the Bio++ Project.
+   This software is a computer program whose purpose is to provide basal and
+   utilitary classes. This file belongs to the Bio++ Project.
 
-This software is governed by the CeCILL  license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
-modify and/ or redistribute the software under the terms of the CeCILL
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+   This software is governed by the CeCILL  license under French law and
+   abiding by the rules of distribution of free software.  You can  use,
+   modify and/ or redistribute the software under the terms of the CeCILL
+   license as circulated by CEA, CNRS and INRIA at the following URL
+   "http://www.cecill.info".
 
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited
-liability. 
+   As a counterpart to the access to the source code and  rights to copy,
+   modify and redistribute granted by the license, users are provided only
+   with a limited warranty  and the software's author,  the holder of the
+   economic rights,  and the successive licensors  have only  limited
+   liability.
 
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+   In this respect, the user's attention is drawn to the risks associated
+   with loading,  using,  modifying and/or developing or reproducing the
+   software by the user in light of its specific status of free software,
+   that may mean  that it is complicated to manipulate,  and  that  also
+   therefore means  that it is reserved for developers  and  experienced
+   professionals having in-depth computer knowledge. Users are therefore
+   encouraged to load and test the software's suitability as regards their
+   requirements in conditions enabling the security of their systems and/or
+   data to be ensured and,  more generally, to use and operate it in the
+   same conditions as regards security.
 
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL license and that you accept its terms.
-*/
+   The fact that you are presently reading this means that you have had
+   knowledge of the CeCILL license and that you accept its terms.
+ */
 
 // From the STL:
 #include <cstdlib>
@@ -59,16 +59,19 @@ std::vector<std::string> AttributesTools::getVector(int argc, char* argv[])
 {
   size_t n = static_cast<size_t>(argc);
   vector<string> result(n);
-  for (size_t i = 1; i < n; ++i) result[i] = string(argv[i]);
-  //Ignore first argc which is the program name!    
+  for (size_t i = 1; i < n; ++i)
+  {
+    result[i] = string(argv[i]);
+  }
+  // Ignore first argc which is the program name!
   return result;
 }
 
 /******************************************************************************/
 
 std::map<std::string, std::string> AttributesTools::getAttributesMap(
-  const std::vector<std::string> & argv,
-  const std::string & delimiter)
+  const std::vector<std::string>& argv,
+  const std::string& delimiter)
 {
   map<string, string> am;
   getAttributesMap(argv, am, delimiter);
@@ -78,37 +81,38 @@ std::map<std::string, std::string> AttributesTools::getAttributesMap(
 /******************************************************************************/
 
 void AttributesTools::getAttributesMap(
-  const std::vector<std::string> & argv,
-  std::map<std::string, std::string> & am,
-  const std::string & delimiter)
+  const std::vector<std::string>& argv,
+  std::map<std::string, std::string>& am,
+  const std::string& delimiter)
 {
   vector<string> argv2(argv.size());
-  //First make a few cleaning:
+  // First make a few cleaning:
   for (size_t i = 0; i < argv.size(); i++)
   {
-    //Make a few corrections first:
-    string arg = removeComments(argv[i], string("#"), string("\n"));//remove shell comments.
-    arg = removeComments(arg, string("//"), string("\n"));//remove C simple comments.
-    arg = removeComments(arg, string("/*"), string("*/"));//remove C multiple comments.
+    // Make a few corrections first:
+    string arg = removeComments(argv[i], string("#"), string("\n")); // remove shell comments.
+    arg = removeComments(arg, string("//"), string("\n")); // remove C simple comments.
+    arg = removeComments(arg, string("/*"), string("*/")); // remove C multiple comments.
     arg = TextTools::removeWhiteSpaces(arg);
     argv2[i] = arg;
   }
-  //Now parse arguments:
+  // Now parse arguments:
   for (size_t i = 0; i < argv.size(); i++)
   {
     string arg = argv2[i];
-    if (arg == "") continue; //Skipping void line.
+    if (arg == "")
+      continue;  // Skipping void line.
     while (arg[arg.size() - 1] == '\\')
     {
-      //Splitted line
+      // Splitted line
       i++;
       arg = arg.substr(0, arg.length() - 1) + argv2[i];
     }
-    //Parsing:
+    // Parsing:
     string::size_type limit = arg.find(delimiter, 0);
     if (limit == string::npos)
     {
-      //Invalid parameter
+      // Invalid parameter
       (*ApplicationTools::warning << "WARNING!!! Parameter '" << arg << "' has been ignored.").endLine();
     }
     else
@@ -120,8 +124,8 @@ void AttributesTools::getAttributesMap(
       //   //Recursive inclusion:
       //   getAttributesMapFromFile(value, am, delimiter);
       // }
-      // else 
-        am[name] = value;
+      // else
+      am[name] = value;
     }
   }
 }
@@ -129,9 +133,9 @@ void AttributesTools::getAttributesMap(
 /******************************************************************************/
 
 void AttributesTools::getAttributesMapFromFile(
-  const std::string & file,
-  std::map<std::string, std::string> & params,
-  const std::string & delimiter)
+  const std::string& file,
+  std::map<std::string, std::string>& params,
+  const std::string& delimiter)
 {
   cout << "Parsing file " << file << " for options." << endl;
   ifstream input(file.c_str(), ios::in);
@@ -142,8 +146,8 @@ void AttributesTools::getAttributesMapFromFile(
 /******************************************************************************/
 
 std::map<std::string, std::string> AttributesTools::getAttributesMapFromFile(
-  const std::string & file,
-  const std::string & delimiter)
+  const std::string& file,
+  const std::string& delimiter)
 {
   map<string, string> params;
   getAttributesMapFromFile(file, params, delimiter);
@@ -153,10 +157,10 @@ std::map<std::string, std::string> AttributesTools::getAttributesMapFromFile(
 /******************************************************************************/
 
 void AttributesTools::actualizeAttributesMap(
-  std::map<std::string, std::string> & attMap,
-  const std::map<std::string, std::string> & atts)
+  std::map<std::string, std::string>& attMap,
+  const std::map<std::string, std::string>& atts)
 {
-  for(map<string, string>::const_iterator i = atts.begin(); i != atts.end(); i++)
+  for (map<string, string>::const_iterator i = atts.begin(); i != atts.end(); i++)
   {
     if ((i->first != "param") && (i->first != "params"))
       attMap[i->first] = i->second;
@@ -166,13 +170,13 @@ void AttributesTools::actualizeAttributesMap(
 /******************************************************************************/
 
 void AttributesTools::resolveVariables(
-  std::map<std::string, std::string> & am,
+  std::map<std::string, std::string>& am,
   char varCode,
   char varBeg,
   char varEnd)
 throw (Exception)
 {
-  //Now resolve any variable:
+  // Now resolve any variable:
   for (map<string, string>::iterator it = am.begin(); it != am.end(); it++)
   {
     string value = it->second;
@@ -185,7 +189,7 @@ throw (Exception)
         string varName  = value.substr(index1 + 2, index2 - index1 - 2);
         map<string, string>::iterator varIt = am.find(varName);
         string varValue = "";
-	if (varIt == am.end())
+        if (varIt == am.end())
         {
           if (ApplicationTools::error)
             (*ApplicationTools::error << "Variable '" << varName << "' is undefined and was ignored.").endLine();
@@ -194,7 +198,7 @@ throw (Exception)
         {
           varValue = varIt->second;
         }
-        //Modify original field:
+        // Modify original field:
         string newValue = value.substr(0, index1) + varValue + value.substr(index2 + 1);
         it->second = newValue;
       }
@@ -218,8 +222,9 @@ std::string AttributesTools::removeComments(
   do
   {
     string::size_type first = r.find(begin, last);
-    if (first == string::npos) return r; //No shell comment.
-    //else:  
+    if (first == string::npos)
+      return r;  // No shell comment.
+    // else:
     last = r.find(end, first);
     if (last == string::npos)
     {
@@ -229,13 +234,14 @@ std::string AttributesTools::removeComments(
     {
       r.erase(r.begin() + static_cast<ptrdiff_t>(first), r.begin() + static_cast<ptrdiff_t>(last));
     }
-  } while (last != string::npos);
+  }
+  while (last != string::npos);
   return r;
 }
 
 /******************************************************************************/
 
-std::map<std::string, std::string> AttributesTools::parseOptions(int args, char ** argv) throw (Exception)
+std::map<std::string, std::string> AttributesTools::parseOptions(int args, char** argv) throw (Exception)
 {
   // Get the parameters from command line:
   map<string, string> cmdParams = AttributesTools::getAttributesMap(
@@ -248,7 +254,7 @@ std::map<std::string, std::string> AttributesTools::parseOptions(int args, char 
   if (cmdParams.find("param") != cmdParams.end())
   {
     string file = cmdParams["param"];
-    if(!FileTools::fileExists(file))
+    if (!FileTools::fileExists(file))
     {
       throw Exception("AttributesTools::parseOptions(). Parameter file not found.");
     }
@@ -269,12 +275,13 @@ std::map<std::string, std::string> AttributesTools::parseOptions(int args, char 
   std::vector<string> mapfile;
   std::vector<string>::iterator imapfile;
   string file;
-  
-  while (true){
-    it=params.find("param");
-    if (it != params.end()) 
+
+  while (true)
+  {
+    it = params.find("param");
+    if (it != params.end())
     {
-      file = it->second;      
+      file = it->second;
       if (std::find(mapfile.begin(), mapfile.end(), file) == mapfile.end())
       {
         params.erase(it);
@@ -289,7 +296,7 @@ std::map<std::string, std::string> AttributesTools::parseOptions(int args, char 
     it = params.find("params");
     if (it != params.end())
     {
-      file = it->second;      
+      file = it->second;
       if (find(mapfile.begin(), mapfile.end(), file) == mapfile.end())
       {
         params.erase(it);
@@ -303,9 +310,8 @@ std::map<std::string, std::string> AttributesTools::parseOptions(int args, char 
     }
     break;
   }
-  
+
   return params;
 }
 
 /******************************************************************************/
-
--- src/Bpp/Utils/AttributesTools.h
+++ src/Bpp/Utils/AttributesTools.h
@@ -5,37 +5,37 @@
 //
 
 /*
-Copyright or © or Copr. Bio++ Development Tools, (November 17, 2004)
+   Copyright or © or Copr. Bio++ Development Tools, (November 17, 2004)
 
-This software is a computer program whose purpose is to provide basal and 
-utilitary classes. This file belongs to the Bio++ Project.
+   This software is a computer program whose purpose is to provide basal and
+   utilitary classes. This file belongs to the Bio++ Project.
 
-This software is governed by the CeCILL  license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
-modify and/ or redistribute the software under the terms of the CeCILL
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+   This software is governed by the CeCILL  license under French law and
+   abiding by the rules of distribution of free software.  You can  use,
+   modify and/ or redistribute the software under the terms of the CeCILL
+   license as circulated by CEA, CNRS and INRIA at the following URL
+   "http://www.cecill.info".
 
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited
-liability. 
+   As a counterpart to the access to the source code and  rights to copy,
+   modify and redistribute granted by the license, users are provided only
+   with a limited warranty  and the software's author,  the holder of the
+   economic rights,  and the successive licensors  have only  limited
+   liability.
 
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+   In this respect, the user's attention is drawn to the risks associated
+   with loading,  using,  modifying and/or developing or reproducing the
+   software by the user in light of its specific status of free software,
+   that may mean  that it is complicated to manipulate,  and  that  also
+   therefore means  that it is reserved for developers  and  experienced
+   professionals having in-depth computer knowledge. Users are therefore
+   encouraged to load and test the software's suitability as regards their
+   requirements in conditions enabling the security of their systems and/or
+   data to be ensured and,  more generally, to use and operate it in the
+   same conditions as regards security.
 
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL license and that you accept its terms.
-*/
+   The fact that you are presently reading this means that you have had
+   knowledge of the CeCILL license and that you accept its terms.
+ */
 
 #ifndef _ATTRIBUTES_TOOLS_H_
 #define _ATTRIBUTES_TOOLS_H_
@@ -48,9 +48,8 @@ knowledge of the CeCILL license and that you accept its terms.
 #include <string>
 #include <vector>
 
-namespace bpp 
+namespace bpp
 {
-
 /**
  * @brief Some functions to deal with attributes, i.e. parameters passed to a program/method.
  *
@@ -72,7 +71,7 @@ namespace bpp
  * @code
  * optionfile=/home/foo/bar.txt
  * @endcode
- * 
+ *
  * Attributes are stored as a map<string, string>, with attributes names as keys,
  * and arguments as values.
  *
@@ -104,7 +103,7 @@ namespace bpp
  * @endcode
  * These pieces of code does the following:
  * - get all parameters from the command line and store them in a map,
- * - check if some parameter is called 'param' or 'params'. If so, look for the file 
+ * - check if some parameter is called 'param' or 'params'. If so, look for the file
  *   given as value and try to read some parameters from it.
  * - If an parameter file was found, update the parameter in it with those from
  *   the command line. This implies that when a parameter is found in both the
@@ -127,125 +126,120 @@ namespace bpp
  */
 class AttributesTools
 {
-  
-  public:
-    
-    AttributesTools() {}
-    virtual ~AttributesTools() {}
-    
-    /**
-     * @brief Get attributes a vector of strings from command line arguments.
-     *
-     * @param argc The number of arguments.
-     * @param argv The array with all arguments.
-     * @return A vector with all arguments as strings.
-     */
-    static std::vector<std::string> getVector(int argc, char * argv[]);
-  
-    /**
-     * @brief Get an attribute map from a vector of arguments.
-     *
-     * This method also resolve all variable calls.
-     *
-     * @param argv      The vector of arguments.
-     * @param delimiter The string that separates attribute names from arguments (for instance '=').
-     * @return          The attribute map.
-     */
-    static std::map<std::string, std::string> getAttributesMap(
-      const std::vector<std::string> & argv,
-      const std::string & delimiter = "=");
+public:
+  AttributesTools() {}
+  virtual ~AttributesTools() {}
 
-    /**
-     * @brief Get an attribute map from a vector of arguments.
-     *
-     * This method also resolve all variable calls.
-     *
-     * @param argv      The vector of arguments.
-     * @param am        The attribute map to fill.
-     * @param delimiter The string that separates attribute names from arguments (for instance '=').
-     */
-    static void getAttributesMap(
-      const std::vector<std::string> & argv,
-      std::map<std::string, std::string> & am,
-      const std::string & delimiter = "=");
+  /**
+   * @brief Get attributes a vector of strings from command line arguments.
+   *
+   * @param argc The number of arguments.
+   * @param argv The array with all arguments.
+   * @return A vector with all arguments as strings.
+   */
+  static std::vector<std::string> getVector(int argc, char* argv[]);
 
-    /**
-     * @brief Get an attribute map from a file.
-     *
-     * @param file      The file with all arguments.
-     * @param delimiter The string that separates attribute names from arguments (for instance '=').
-     * @return An attribute map.
-     */
-    static std::map<std::string, std::string> getAttributesMapFromFile(
-      const std::string & file,
-      const std::string & delimiter);
-    
-    /**
-     * @brief Get an attribute map from a file.
-     *
-     * @param file      The file with all arguments.
-     * @param params    An attribute map to fill.
-     * @param delimiter The string that separates attribute names from arguments (for instance '=').
-     */
-    static void getAttributesMapFromFile(
-      const std::string & file,
-      std::map<std::string, std::string> & params,
-      const std::string & delimiter);
-  
-    /**
-     * @brief Actualizes an attribute map with another.
-     *
-     * All fields in map 2 will be added to map 1.
-     * If two attributes have the same name, then map 1 value will be overwritten.
-     *
-     * @param attMap The attributes map.
-     * @param atts   The attributes to add to the map.
-     */
-    static void actualizeAttributesMap(
-      std::map<std::string, std::string> & attMap,
-      const std::map<std::string, std::string> & atts);
-    
-    /**
-     * @brief Resolve the variables.
-     *
-     * If used prior to the actualizeAttributesMap, this function will make the
-     * variables 'local', whereas using them after will make them 'global'.
-     *
-     * @param am The attributes map.
-     * @param varCode   The code that defines variable recalls.
-     * @param varBeg    Variables begin name code.
-     * @param varEnd    Variables end name code.
-     * @throw Exception If there is a syntax error.
-     */
-    static void resolveVariables(std::map<std::string, std::string> & am,
-      char varCode = '$',
-      char varBeg = '(',
-      char varEnd = ')') throw (Exception);
+  /**
+   * @brief Get an attribute map from a vector of arguments.
+   *
+   * This method also resolve all variable calls.
+   *
+   * @param argv      The vector of arguments.
+   * @param delimiter The string that separates attribute names from arguments (for instance '=').
+   * @return          The attribute map.
+   */
+  static std::map<std::string, std::string> getAttributesMap(
+    const std::vector<std::string>& argv,
+    const std::string& delimiter = "=");
 
-    /**
-     * @brief Global function that reads all parameters from command line and files,
-     * and set the values in a map.
-     *
-     * @param args Number of arguments, as passed to the main function.
-     * @param argv Array of values, as passed to the main function.
-     * @return An attributes map.
-     * @throw Exception in case an option file is not found.
-     */
-    static std::map<std::string, std::string> parseOptions(int args, char ** argv) throw (Exception);
+  /**
+   * @brief Get an attribute map from a vector of arguments.
+   *
+   * This method also resolve all variable calls.
+   *
+   * @param argv      The vector of arguments.
+   * @param am        The attribute map to fill.
+   * @param delimiter The string that separates attribute names from arguments (for instance '=').
+   */
+  static void getAttributesMap(
+    const std::vector<std::string>& argv,
+    std::map<std::string, std::string>& am,
+    const std::string& delimiter = "=");
 
-  private:
-    
-    /**
-     * @brief Remove comments from a string.
-     *
-     * @param s     The string to parse.
-     * @param begin Comments front delimiter.
-     * @param end   Comments end delimiter.
-     */
-    static std::string removeComments(const std::string & s, const std::string & begin, const std::string & end);
+  /**
+   * @brief Get an attribute map from a file.
+   *
+   * @param file      The file with all arguments.
+   * @param delimiter The string that separates attribute names from arguments (for instance '=').
+   * @return An attribute map.
+   */
+  static std::map<std::string, std::string> getAttributesMapFromFile(
+    const std::string& file,
+    const std::string& delimiter);
+
+  /**
+   * @brief Get an attribute map from a file.
+   *
+   * @param file      The file with all arguments.
+   * @param params    An attribute map to fill.
+   * @param delimiter The string that separates attribute names from arguments (for instance '=').
+   */
+  static void getAttributesMapFromFile(
+    const std::string& file,
+    std::map<std::string, std::string>& params,
+    const std::string& delimiter);
+
+  /**
+   * @brief Actualizes an attribute map with another.
+   *
+   * All fields in map 2 will be added to map 1.
+   * If two attributes have the same name, then map 1 value will be overwritten.
+   *
+   * @param attMap The attributes map.
+   * @param atts   The attributes to add to the map.
+   */
+  static void actualizeAttributesMap(
+    std::map<std::string, std::string>& attMap,
+    const std::map<std::string, std::string>& atts);
+
+  /**
+   * @brief Resolve the variables.
+   *
+   * If used prior to the actualizeAttributesMap, this function will make the
+   * variables 'local', whereas using them after will make them 'global'.
+   *
+   * @param am The attributes map.
+   * @param varCode   The code that defines variable recalls.
+   * @param varBeg    Variables begin name code.
+   * @param varEnd    Variables end name code.
+   * @throw Exception If there is a syntax error.
+   */
+  static void resolveVariables(std::map<std::string, std::string>& am,
+                               char varCode = '$',
+                               char varBeg = '(',
+                               char varEnd = ')') throw (Exception);
+
+  /**
+   * @brief Global function that reads all parameters from command line and files,
+   * and set the values in a map.
+   *
+   * @param args Number of arguments, as passed to the main function.
+   * @param argv Array of values, as passed to the main function.
+   * @return An attributes map.
+   * @throw Exception in case an option file is not found.
+   */
+  static std::map<std::string, std::string> parseOptions(int args, char** argv) throw (Exception);
+
+private:
+  /**
+   * @brief Remove comments from a string.
+   *
+   * @param s     The string to parse.
+   * @param begin Comments front delimiter.
+   * @param end   Comments end delimiter.
+   */
+  static std::string removeComments(const std::string& s, const std::string& begin, const std::string& end);
 };
-
-} //end of namespace bpp.
+} // end of namespace bpp.
 
 #endif // _ATTRIBUTES_TOOLS_H_
-
