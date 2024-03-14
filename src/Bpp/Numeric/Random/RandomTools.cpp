735a054c0f634355e87cee7595c6f4ab2b0357ec
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2024-03-14 21:01:36 +0100

    Added license using "reuse.software"

--- src/Bpp/Numeric/Random/RandomTools.cpp
+++ src/Bpp/Numeric/Random/RandomTools.cpp
@@ -1,37 +1,4 @@
 
-/*
-  Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
-  
-  This software is a computer program whose purpose is to provide classes
-  for numerical calculus.
-  
-  This software is governed by the CeCILL license under French law and
-  abiding by the rules of distribution of free software. You can use,
-  modify and/ or redistribute the software under the terms of the CeCILL
-  license as circulated by CEA, CNRS and INRIA at the following URL
-  "http://www.cecill.info".
-  
-  As a counterpart to the access to the source code and rights to copy,
-  modify and redistribute granted by the license, users are provided only
-  with a limited warranty and the software's author, the holder of the
-  economic rights, and the successive licensors have only limited
-  liability.
-  
-  In this respect, the user's attention is drawn to the risks associated
-  with loading, using, modifying and/or developing or reproducing the
-  software by the user in light of its specific status of free software,
-  that may mean that it is complicated to manipulate, and that also
-  therefore means that it is reserved for developers and experienced
-  professionals having in-depth computer knowledge. Users are therefore
-  encouraged to load and test the software's suitability as regards their
-  requirements in conditions enabling the security of their systems and/or
-  data to be ensured and, more generally, to use and operate it in the
-  same conditions as regards security.
-  
-  The fact that you are presently reading this means that you have had
-  knowledge of the CeCILL license and that you accept its terms.
-*/
-
 #include <iostream>
 
 #include "../NumConstants.h"
@@ -784,14 +751,11 @@ double RandomTools::incompletebetafe(double a,
   return result;
 }
 
+// Copyright 1984, 1995, 2000 by Stephen L. Moshier
+// SPDX-FileCopyrightText: The Bio++ Development Group
+//
+// SPDX-License-Identifier: CECILL-2.1
 
-/*************************************************************************
-   Continued fraction expansion #2
-   for incomplete beta integral
-
-   Cephes Math Library, Release 2.8:  June, 2000
-   Copyright 1984, 1995, 2000 by Stephen L. Moshier
-*************************************************************************/
 double RandomTools::incompletebetafe2(double a,
                                       double b,
                                       double x,
